88be1bf24e59dc66a74e45bc0118a86a7d790f5c
DUC-TQ <81822869+NHD-TQ@users.noreply.github.com>
2024-04-22 10:12:37 +0700

    upload

---
+++ process.py
@@ -0,0 +1,443 @@
+from llama_index.query_pipeline import (
+    QueryPipeline as QP,
+    Link,
+    InputComponent,
+)
+from llama_index.query_engine.pandas import PandasInstructionParser
+from llama_index.llms import OpenAI
+from llama_index.prompts import PromptTemplate
+import os
+import pandas as pd
+from pathlib import Path
+
+
+# define api_key
+os.environ["OPENAI_API_KEY"] = "sk-proj-3NP4JfovJ9Uw6GuXKEzAT3BlbkFJUeehocmaT92Omte7MxTp"
+
+# READ DATA.
+data_dir = Path("data")
+csv_files = sorted([f for f in data_dir.glob("*.csv")])
+dfs = []
+for csv_file in csv_files:
+    print(f"processing file: {csv_file}")
+    try:
+        df = pd.read_csv(csv_file)
+        dfs.append(df)
+    except Exception as e:
+        print(f"Error parsing {csv_file}: {str(e)}")
+
+from llama_index.program import LLMTextCompletionProgram
+from llama_index.bridge.pydantic import BaseModel, Field
+from llama_index.llms import OpenAI
+
+
+class TableInfo(BaseModel):
+    """Information regarding a structured table."""
+
+    table_name: str = Field(
+        ..., description="table name (must be underscores and NO spaces)"
+    )
+    table_summary: str = Field(
+        ..., description="short, concise summary/caption of the table"
+    )
+
+# GET INFO TABLE USING LLM (SUMMARY TABLE).
+prompt_str = """\
+Give me a summary of the table with the following JSON format.
+
+- The table name must be unique to the table and describe it while being concise.
+- Do NOT output a generic table name (e.g. table, my_table).
+
+Do NOT make the table name one of the following: {exclude_table_name_list}
+
+Table:
+{table_str}
+
+Summary: """
+
+program = LLMTextCompletionProgram.from_defaults(
+    output_cls=TableInfo,
+    llm=OpenAI(model="gpt-3.5-turbo"),
+    prompt_template_str=prompt_str,
+)
+
+# LẤY CÁC TÓM TẮT BẢNG VỚI CHỈ SỐ CHO TRƯỚC.
+import json
+
+def _get_tableinfo_with_index(idx: int) -> str:
+    results_gen = Path(tableinfo_dir).glob(f"{idx}_*")
+    results_list = list(results_gen)
+    if len(results_list) == 0:
+        return None
+    elif len(results_list) == 1:
+        path = results_list[0]
+        return TableInfo.parse_file(path)
+    else:
+        raise ValueError(
+            f"More than one file matching index: {list(results_gen)}"
+        )
+
+tableinfo_dir = "Chatbot_TableInfo"
+table_names = set()
+table_infos = []
+for idx, df in enumerate(dfs):
+    table_info = _get_tableinfo_with_index(idx)
+    if table_info:
+        table_infos.append(table_info)
+    else:
+        while True:
+            df_str = df.head(10).to_csv()
+            table_info = program(
+                table_str=df_str,
+                exclude_table_name_list=str(list(table_names)),
+            )
+            table_name = table_info.table_name
+            print(f"Processed table: {table_name}")
+            if table_name not in table_names:
+                table_names.add(table_name)
+                break
+            else:
+                # try again
+                print(f"Table name {table_name} already exists, trying again.")
+                pass
+
+        out_file = f"{tableinfo_dir}/{idx}_{table_name}.json"
+        json.dump(table_info.dict(), open(out_file, "w"))
+    table_infos.append(table_info)
+
+# put data into sqlite db
+from sqlalchemy import (
+    create_engine,
+    MetaData,
+    Table,
+    Column,
+    String,
+    Integer,
+)
+import re
+
+
+# Function to create a sanitized column name
+def sanitize_column_name(col_name):
+    # Remove special characters and replace spaces with underscores
+    return re.sub(r"\W+", "_", col_name)
+
+
+# Function to create a table from a DataFrame using SQLAlchemy
+def create_table_from_dataframe(
+    df: pd.DataFrame, table_name: str, engine, metadata_obj
+):
+    # Sanitize column names
+    sanitized_columns = {col: sanitize_column_name(col) for col in df.columns}
+    df = df.rename(columns=sanitized_columns)
+
+    # Dynamically create columns based on DataFrame columns and data types
+    columns = [
+        Column(col, String if dtype == "object" else Integer)
+        for col, dtype in zip(df.columns, df.dtypes)
+    ]
+
+    # Create a table with the defined columns
+    table = Table(table_name, metadata_obj, *columns)
+
+    # Create the table in the database
+    metadata_obj.create_all(engine)
+
+    # Insert data from DataFrame into the table
+    with engine.connect() as conn:
+        for _, row in df.iterrows():
+            insert_stmt = table.insert().values(**row.to_dict())
+            conn.execute(insert_stmt)
+        conn.commit()
+
+
+engine = create_engine("sqlite:///:memory:")
+metadata_obj = MetaData()
+for idx, df in enumerate(dfs):
+    tableinfo = _get_tableinfo_with_index(idx)
+    print(f"Creating table: {tableinfo.table_name}")
+    create_table_from_dataframe(df, tableinfo.table_name, engine, metadata_obj)
+
+# setup Arize Phoenix for logging/observability
+import phoenix as px
+import llama_index
+
+px.launch_app()
+llama_index.set_global_handler("arize_phoenix")
+
+from llama_index.objects import (
+    SQLTableNodeMapping,
+    ObjectIndex,
+    SQLTableSchema,
+)
+from llama_index import SQLDatabase, VectorStoreIndex
+
+sql_database = SQLDatabase(engine)
+
+table_node_mapping = SQLTableNodeMapping(sql_database)
+table_schema_objs = [
+    SQLTableSchema(table_name=t.table_name, context_str=t.table_summary)
+    for t in table_infos
+]  # add a SQLTableSchema for each table
+
+obj_index = ObjectIndex.from_objects(
+    table_schema_objs,
+    table_node_mapping,
+    VectorStoreIndex,
+)
+obj_retriever = obj_index.as_retriever(similarity_top_k=3)
+
+from llama_index.retrievers import SQLRetriever
+from typing import List
+from llama_index.query_pipeline import FnComponent
+
+sql_retriever = SQLRetriever(sql_database)
+
+
+def get_table_context_str(table_schema_objs: List[SQLTableSchema]):
+    """Get table context string."""
+    context_strs = []
+    for table_schema_obj in table_schema_objs:
+        table_info = sql_database.get_single_table_info(
+            table_schema_obj.table_name
+        )
+        if table_schema_obj.context_str:
+            table_opt_context = " The table description is: "
+            table_opt_context += table_schema_obj.context_str
+            table_info += table_opt_context
+
+        context_strs.append(table_info)
+    return "\n\n".join(context_strs)
+
+
+table_parser_component = FnComponent(fn=get_table_context_str)
+
+
+from llama_index.prompts.default_prompts import DEFAULT_TEXT_TO_SQL_PROMPT
+from llama_index.prompts import PromptTemplate
+from llama_index.query_pipeline import FnComponent
+from llama_index.llms import ChatResponse
+
+
+def parse_response_to_sql(response: ChatResponse) -> str:
+    """Parse response to SQL."""
+    response = response.message.content
+    sql_query_start = response.find("SQLQuery:")
+    if sql_query_start != -1:
+        response = response[sql_query_start:]
+        # TODO: move to removeprefix after Python 3.9+
+        if response.startswith("SQLQuery:"):
+            response = response[len("SQLQuery:") :]
+    sql_result_start = response.find("SQLResult:")
+    if sql_result_start != -1:
+        response = response[:sql_result_start]
+    return response.strip().strip("```").strip()
+
+
+sql_parser_component = FnComponent(fn=parse_response_to_sql)
+
+text2sql_prompt = DEFAULT_TEXT_TO_SQL_PROMPT.partial_format(
+    dialect=engine.dialect.name
+)
+print(text2sql_prompt.template)
+
+response_synthesis_prompt_str = (
+    "Given an input question, synthesize a response from the query results.\n"
+    "Query: {query_str}\n"
+    "SQL: {sql_query}\n"
+    "SQL Response: {context_str}\n"
+    "If the query is not valid for pandas dataframe then respond like a sales consultant.\n"
+    "You must answer all questions in Vietnamese.\n"
+    "Response: "
+)
+response_synthesis_prompt = PromptTemplate(
+    response_synthesis_prompt_str,
+)
+
+llm = OpenAI(model="gpt-3.5-turbo")
+
+from llama_index.query_pipeline import (
+    QueryPipeline as QP,
+    Link,
+    InputComponent,
+    CustomQueryComponent,
+)
+
+qp = QP(
+    modules={
+        "input": InputComponent(),
+        "table_retriever": obj_retriever,
+        "table_output_parser": table_parser_component,
+        "text2sql_prompt": text2sql_prompt,
+        "text2sql_llm": llm,
+        "sql_output_parser": sql_parser_component,
+        "sql_retriever": sql_retriever,
+        "response_synthesis_prompt": response_synthesis_prompt,
+        "response_synthesis_llm": llm,
+    },
+    verbose=True,
+)
+
+qp.add_chain(["input", "table_retriever", "table_output_parser"])
+qp.add_link("input", "text2sql_prompt", dest_key="query_str")
+qp.add_link("table_output_parser", "text2sql_prompt", dest_key="schema")
+qp.add_chain(
+    ["text2sql_prompt", "text2sql_llm", "sql_output_parser", "sql_retriever"]
+)
+qp.add_link(
+    "sql_output_parser", "response_synthesis_prompt", dest_key="sql_query"
+)
+qp.add_link(
+    "sql_retriever", "response_synthesis_prompt", dest_key="context_str"
+)
+qp.add_link("input", "response_synthesis_prompt", dest_key="query_str")
+qp.add_link("response_synthesis_prompt", "response_synthesis_llm")
+
+# response = qp.run(
+#     query="Please advise me on some induction cooker models with a kitchen surface size of 345x270 mm?"
+# )
+# print(str(response))
+
+###################
+from llama_index.query_pipeline import QueryPipeline as QP
+from llama_index.service_context import ServiceContext
+
+qp = QP(verbose=True)
+# NOTE: service context will be deprecated in v0.10 (though will still be backwards compatible)
+service_context = ServiceContext.from_defaults(callback_manager=qp.callback_manager)
+
+from llama_index import VectorStoreIndex, load_index_from_storage
+from sqlalchemy import text
+from llama_index.schema import TextNode
+from llama_index.storage import StorageContext
+import os
+from pathlib import Path
+from typing import Dict
+
+
+def index_all_tables(
+    sql_database: SQLDatabase, table_index_dir: str = "table_index_dir"
+) -> Dict[str, VectorStoreIndex]:
+    """Index all tables."""
+    if not Path(table_index_dir).exists():
+        os.makedirs(table_index_dir)
+
+    vector_index_dict = {}
+    engine = sql_database.engine
+    for table_name in sql_database.get_usable_table_names():
+        print(f"Indexing rows in table: {table_name}")
+        if not os.path.exists(f"{table_index_dir}/{table_name}"):
+            # get all rows from table
+            with engine.connect() as conn:
+                cursor = conn.execute(text(f'SELECT * FROM "{table_name}"'))
+                result = cursor.fetchall()
+                row_tups = []
+                for row in result:
+                    row_tups.append(tuple(row))
+
+            # index each row, put into vector store index
+            nodes = [TextNode(text=str(t)) for t in row_tups]
+
+            # put into vector store index (use OpenAIEmbeddings by default)
+            index = VectorStoreIndex(nodes, service_context=service_context)
+
+            # save index
+            index.set_index_id("vector_index")
+            index.storage_context.persist(f"{table_index_dir}/{table_name}")
+        else:
+            # rebuild storage context
+            storage_context = StorageContext.from_defaults(
+                persist_dir=f"{table_index_dir}/{table_name}"
+            )
+            # load index
+            index = load_index_from_storage(
+                storage_context, index_id="vector_index", service_context=service_context
+            )
+        vector_index_dict[table_name] = index
+
+    return vector_index_dict
+
+
+vector_index_dict = index_all_tables(sql_database)
+
+from llama_index.retrievers import SQLRetriever
+from typing import List
+from llama_index.query_pipeline import FnComponent
+
+sql_retriever = SQLRetriever(sql_database)
+
+
+def get_table_context_and_rows_str(
+    query_str: str, table_schema_objs: List[SQLTableSchema]
+):
+    """Get table context string."""
+    context_strs = []
+    for table_schema_obj in table_schema_objs:
+        # first append table info + additional context
+        table_info = sql_database.get_single_table_info(
+            table_schema_obj.table_name
+        )
+        if table_schema_obj.context_str:
+            table_opt_context = " The table description is: "
+            table_opt_context += table_schema_obj.context_str
+            table_info += table_opt_context
+
+        # also lookup vector index to return relevant table rows
+        vector_retriever = vector_index_dict[
+            table_schema_obj.table_name
+        ].as_retriever(similarity_top_k=2)
+        relevant_nodes = vector_retriever.retrieve(query_str)
+        if len(relevant_nodes) > 0:
+            table_row_context = "\nHere are some relevant example rows (values in the same order as columns above)\n"
+            for node in relevant_nodes:
+                table_row_context += str(node.get_content()) + "\n"
+            table_info += table_row_context
+
+        context_strs.append(table_info)
+    return "\n\n".join(context_strs)
+
+
+table_parser_component = FnComponent(fn=get_table_context_and_rows_str)
+
+from llama_index.query_pipeline import (
+    QueryPipeline as QP,
+    Link,
+    InputComponent,
+    CustomQueryComponent,
+)
+
+qp.add_modules({
+    "input": InputComponent(),
+    "table_retriever": obj_retriever,
+    "table_output_parser": table_parser_component,
+    "text2sql_prompt": text2sql_prompt,
+    "text2sql_llm": llm,
+    "sql_output_parser": sql_parser_component,
+    "sql_retriever": sql_retriever,
+    "response_synthesis_prompt": response_synthesis_prompt,
+    "response_synthesis_llm": llm,
+})
+
+qp.add_link("input", "table_retriever")
+qp.add_link("input", "table_output_parser", dest_key="query_str")
+qp.add_link(
+    "table_retriever", "table_output_parser", dest_key="table_schema_objs"
+)
+qp.add_link("input", "text2sql_prompt", dest_key="query_str")
+qp.add_link("table_output_parser", "text2sql_prompt", dest_key="schema")
+qp.add_chain(
+    ["text2sql_prompt", "text2sql_llm", "sql_output_parser", "sql_retriever"]
+)
+qp.add_link(
+    "sql_output_parser", "response_synthesis_prompt", dest_key="sql_query"
+)
+qp.add_link(
+    "sql_retriever", "response_synthesis_prompt", dest_key="context_str"
+)
+qp.add_link("input", "response_synthesis_prompt", dest_key="query_str")
+qp.add_link("response_synthesis_prompt", "response_synthesis_llm")
+
+response = qp.run(
+    query="Please advise me on some induction cooker models with a kitchen surface size of 345x270 mm?"
+)
+print(str(response))
